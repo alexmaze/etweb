9805c206d4a8dd68e1b3644089d700d8bc801948
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2019-04-11 16:58:52 +0800

    add deploy script

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: "etweb-main",
+      script: "dist/main.js",
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 4,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "47.99.63.21",
+      key: "~/.ssh/id_rsa",
+      ref: "origin/master",
+      repo: "git@github.com:yanhao1991/etweb.git",
+      path: "/var/www/etweb",
+      "post-deploy":
+        "npm run start:prod && pm2 reload ecosystem.config.js --env production"
+    }
+  }
+}
