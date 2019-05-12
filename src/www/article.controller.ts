9cd6ff63c8704809f447304d00b641bad6db6143
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2019-05-12 16:38:06 +0800

    finish article page

--- src/www/article.controller.ts
+++ src/www/article.controller.ts
@@ -11,6 +11,7 @@ import { LanguageType, VariableKeys } from "../main/variable.entity"
 import { ETWEB_LANGUAGE } from "./language.middleware"
 import { WebPosition, CommonService } from "./services/common.service"
 import { ArticleService } from "src/main/article.service"
+import { ArticleType } from "src/main/article.entity"
 
 @Controller("/article")
 export class ArticleController {
@@ -25,10 +26,50 @@ export class ArticleController {
   async index(@Headers(ETWEB_LANGUAGE) lang: LanguageType) {
     const common = await this.commonServ.getCommonData(lang, WebPosition.News)
 
+    const news = await this.articleServ.list(
+      { page: 1, size: 2 },
+      ArticleType.News,
+      lang
+    )
+    const shares = await this.articleServ.list(
+      { page: 1, size: 5 },
+      ArticleType.Share,
+      lang
+    )
+
+    news.data.forEach((item: any) => {
+      if (item.content) {
+        item.content = item.content.replace(/<[^>]+>|&[^>]+;/g, "").trim()
+        item._createdAt = item.createdAt
+          .toISOString()
+          .substr(0, 10)
+          .substr(0, 100)
+      }
+    })
+    shares.data.forEach((item: any) => {
+      if (item.content) {
+        item.content = item.content.replace(/<[^>]+>|&[^>]+;/g, "").trim()
+        item._createdAt = item.createdAt
+          .toISOString()
+          .substr(0, 10)
+          .substr(0, 100)
+      }
+    })
+
+    const [s1, s2, s3, ...otherShares] = shares.data
+
     const ret = {
       ...common,
-      pageTitle: lang === LanguageType.English ? "News & Share" : "新闻资讯"
-    }
+      pageTitle: lang === LanguageType.English ? "News & Share" : "新闻资讯",
+      news: news.data,
+      sharesTop: [s1, s2, s3],
+      sharesLeft: otherShares,
+      _share: lang === LanguageType.English ? "Shares" : "经验分享",
+      _more: lang === LanguageType.English ? "More" : "加载更多",
+      _top: lang === LanguageType.English ? "STICK" : "置顶"
+    } as any
+
+    ret._data = JSON.stringify(ret)
 
     return ret
   }
@@ -41,6 +82,9 @@ export class ArticleController {
   ) {
     const common = await this.commonServ.getCommonData(lang, WebPosition.News)
     const data = await this.articleServ.show(id)
+
+    data.viewCount++
+    this.articleServ.update(data)
 
     const ret = {
       ...common,
