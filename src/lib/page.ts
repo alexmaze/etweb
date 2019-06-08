c19c08149c2f432025b6400bc589f1e8ecfdf779
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2019-06-08 12:02:24 +0800

    sort by pin

--- src/lib/page.ts
+++ src/lib/page.ts
@@ -40,4 +40,36 @@ export class Pager<T> {
       data
     }
   }
+
+  async getPage2(
+    where?: any,
+    sort?: string,
+    order?: "ASC" | "DESC",
+    sort2?: string,
+    order2?: "ASC" | "DESC"
+  ): Promise<IPageRes<T>> {
+    const skip = (this.params.page - 1) * this.params.size
+
+    let query = this.repo.createQueryBuilder("r")
+
+    if (sort != null) {
+      query = query.addOrderBy(sort, order || "DESC")
+    }
+    if (sort2 != null) {
+      query = query.addOrderBy(sort2, order2 || "DESC")
+    }
+
+    query = query
+      .where(where)
+      .offset(skip)
+      .limit(this.params.size)
+
+    const [data, total] = await query.getManyAndCount()
+
+    return {
+      ...this.params,
+      total,
+      data
+    }
+  }
 }
